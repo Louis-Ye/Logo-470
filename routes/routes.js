63527db01fe5bbc971fe8f5233ceb5883b0b4af6
ywa165 <ywa165@5506eaa1-8d03-4ba1-8c37-6516f2293ca1>
2014-03-22 23:54:23 +0000

    git-svn-id: https://punch.cs.sfu.ca/svn/CMPT470-1141-g-gem@4 5506eaa1-8d03-4ba1-8c37-6516f2293ca1

--- routes/routes.js
+++ routes/routes.js
@@ -34,6 +34,47 @@ module.exports = function(app, passport) {
 		failureRedirect : '/signup', // redirect back to the signup page if there is an error
 		failureFlash : true // allow flash messages
 	}));
+
+	// facebook 
+	app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+
+	// handle the callback after facebook has authenticated the user
+	app.get('/auth/facebook/callback',
+		passport.authenticate('facebook', {
+			successRedirect : '/profile',
+			failureRedirect : '/'
+		})
+	);
+
+
+	app.get('/connect/local', function(req, res) {
+		res.render('connect-local.ejs', { message: req.flash('loginMessage') });
+	});
+	
+	app.post('/connect/local', passport.authenticate('local-signup', {
+		successRedirect : '/profile', // redirect to the secure profile section
+		failureRedirect : '/connect/local', // redirect back to the signup page if there is an error
+		failureFlash : true // allow flash messages
+	}));
+
+	// send to facebook to do the authentication
+	app.get('/connect/facebook', passport.authorize('facebook', { scope : 'email' }));
+
+	// handle the callback after facebook has authorized the user
+	app.get('/connect/facebook/callback',
+		passport.authorize('facebook', {
+			successRedirect : '/profile',
+			failureRedirect : '/'
+	}));
+
+	// facebook -------------------------------
+	app.get('/unlink/facebook', function(req, res) {
+		var user            = req.user;
+		user.facebook.token = undefined;
+		user.save(function(err) {
+			res.redirect('/profile');
+		});
+	});
 };
 
 // route middleware to ensure user is logged in
