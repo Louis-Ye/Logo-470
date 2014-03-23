c3408b7d6dc13d5c1f836c8b5947715e103974f5
ywa165 <ywa165@5506eaa1-8d03-4ba1-8c37-6516f2293ca1>
2014-03-23 00:21:40 +0000

    git-svn-id: https://punch.cs.sfu.ca/svn/CMPT470-1141-g-gem@6 5506eaa1-8d03-4ba1-8c37-6516f2293ca1

--- models/user.js
+++ models/user.js
@@ -12,8 +12,9 @@ var userSchema = mongoose.Schema({
         id           : String,
         token        : String,
         email        : String,
-        name         : String
-    }
+        name         : String,
+    },
+    register : String
 });
 
 // generating a hash
