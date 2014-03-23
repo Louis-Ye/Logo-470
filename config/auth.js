d735c6cf76c15e94adfa680bf5e16150df0dd553
ywa165 <ywa165@5506eaa1-8d03-4ba1-8c37-6516f2293ca1>
2014-03-23 00:07:10 +0000

    git-svn-id: https://punch.cs.sfu.ca/svn/CMPT470-1141-g-gem@5 5506eaa1-8d03-4ba1-8c37-6516f2293ca1

---
+++ config/auth.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+	'facebookAuth' : {
+		'clientID' 		: '611459898943858', // your App ID
+		'clientSecret' 	: '7bb2e93aac0e5c73534d8df3b9f9d398', // your App Secret
+		'callbackURL' 	: 'http://localhost:3000/auth/facebook/callback'
+	}
+};
