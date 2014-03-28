2ae0587cfb891c51f773b03d7dd51b961916f7fc
caoshanc <caoshanc@5506eaa1-8d03-4ba1-8c37-6516f2293ca1>
2014-03-28 06:21:39 +0000

    import front end code
    git-svn-id: https://punch.cs.sfu.ca/svn/CMPT470-1141-g-gem@16 5506eaa1-8d03-4ba1-8c37-6516f2293ca1

---
+++ public/app/app.js
@@ -0,0 +1,32 @@
+var ExpressLOGOApp = angular.module('ExpressLOGOApp', ['ngRoute', 'ngAnimate']);
+
+ExpressLOGOApp.config(function ($routeProvider) {
+	$routeProvider
+		.when('/', {
+			controller: 'indexViewController',
+			templateUrl: 'app/partials/homepage.html'
+		})
+		.when('/learn', {
+			controller: 'learnViewController',
+			templateUrl: 'app/partials/learn.html'
+		})
+		.when('/play', {
+			controller: 'playViewController',
+			templateUrl: 'app/partials/play.html'
+		})
+		.when('/library', {
+			controller: 'libraryViewController',
+			templateUrl: 'app/partials/library.html'
+		})
+		.when('/gallery', {
+			controller: 'galleryViewController',
+			templateUrl: 'app/partials/gallery.html'
+		})
+		.when('/about', {
+			controller: 'aboutViewController',
+			templateUrl: 'app/partials/about.html'
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
+});
---
+++ public/app/controllers/controllers.js
@@ -0,0 +1,54 @@
+ExpressLOGOApp.controller('indexViewController', function ($scope) {
+	init();
+	function init() {
+
+	};
+});
+
+ExpressLOGOApp.controller('learnViewController', function ($scope) {
+	init();
+	function init() {
+
+	};
+});
+
+ExpressLOGOApp.controller('playViewController', function ($scope) {
+	init();
+	function init() {
+
+	};
+
+	var callback = function (result) {
+		// show result in upper box
+	};
+
+	var userTyping = text in lower box;
+
+	interpret({
+		'userTyping': userTyping,
+		'delay': 0.1,
+		'debugMode': true,
+		'callback': callback
+	});
+});
+
+ExpressLOGOApp.controller('libraryViewController', function ($scope) {
+	init();
+	function init() {
+
+	};
+});
+
+ExpressLOGOApp.controller('galleryViewController', function ($scope) {
+	init();
+	function init() {
+
+	};
+});
+
+ExpressLOGOApp.controller('aboutViewController', function ($scope) {
+	init();
+	function init() {
+
+	};
+});
