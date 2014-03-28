6a0882eb0e343d6ae7eab12be7c2387f7c89c786
caoshanc <caoshanc@5506eaa1-8d03-4ba1-8c37-6516f2293ca1>
2014-03-28 06:21:59 +0000

    import front end code
    git-svn-id: https://punch.cs.sfu.ca/svn/CMPT470-1141-g-gem@17 5506eaa1-8d03-4ba1-8c37-6516f2293ca1

--- app.js
+++ app.js
@@ -9,16 +9,16 @@ var flash = require('connect-flash');
 
 mongoose.connect('mongodb://localhost/logo');
 
-require('./config/passport')(passport); 
+require('./config/passport')(passport);
 require('./config/passport-facebook')(passport);
 
 app.configure(function() {
-
+	app.use(express.static(__dirname + '/public'));
 	app.set('port', process.env.PORT || 3000);
 	app.set('views', path.join(__dirname, 'views'));
-	app.use(express.logger('dev')); 
-	app.use(express.cookieParser()); 
-	app.use(express.bodyParser()); 
+	app.use(express.logger('dev'));
+	app.use(express.cookieParser());
+	app.use(express.bodyParser());
 	app.use(express.json());
 	app.use(express.urlencoded());
 	app.use(express.methodOverride());
