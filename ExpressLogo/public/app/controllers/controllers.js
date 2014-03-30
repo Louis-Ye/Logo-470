91cc25cebc3000914f2c6763ff254bccae554b3b
caoshanc <caoshanc@5506eaa1-8d03-4ba1-8c37-6516f2293ca1>
2014-03-30 07:46:27 +0000

    implement pen control and share
    git-svn-id: https://punch.cs.sfu.ca/svn/CMPT470-1141-g-gem@85 5506eaa1-8d03-4ba1-8c37-6516f2293ca1

--- ExpressLogo/public/app/controllers/controllers.js
+++ ExpressLogo/public/app/controllers/controllers.js
@@ -13,58 +13,72 @@ ExpressLOGOApp.controller('learnViewController', function ($scope) {
 });
 
 ExpressLOGOApp.controller('playViewController', function ($scope) {
+	var interpret_json;
 	init();
 
-	function init() {
-		$scope.result = "";
-		document.getElementById("code-pad").focus();
-		initCanvas();
-	};
-
-	var callback = function (message, command) {
+	function callback (message) {
 		if (message) {
 			$scope.result += message + '\n';
 			$("#result-pad").scrollTop(999);
-		}
-		else {
-			newInputData(command.instruction, command.value);
-		}
+		};
 	};
 
-	$scope.on_submit_clicked = function () {
-		callback($scope.code, null);
-
-		interpret({
+	function init() {
+		$scope.result = "";
+		$scope.pen_status = myCanvas.getDrawStatus();
+		if ($scope.pen_status) {
+			$scope.pen_string = "up";
+		} else {
+			$scope.pen_string = "down";
+		}
+		document.getElementById("code-pad").focus();
+		myCanvas.initCanvas();
+		interpret_json = {
 			'userTyping': $scope.code,
 			'delay': 1,
 			'debugMode': false,
 			'callback': callback
-		});
+		};
+	};
 
+	$scope.on_submit_clicked = function () {
+		callback($scope.code);
+		interpret_json.userTyping = $scope.code;
+		interpret(interpret_json);
 		$scope.code = "";
 		document.getElementById("code-pad").focus();
 	};
 
-    $scope.toggle_pen = function () {
-        // pen.up = !pen.up;
-        if (pen.up) {};
-    };
+	$scope.toggle_pen = function () {
+		$scope.pen_status = myCanvas.getDrawStatus();
+		if ($scope.pen_status) {
+			interpret_json.userTyping = "penup";
+			$scope.pen_string = "down";
+		}
+		else {
+			interpret_json.userTyping = "pendown";
+			$scope.pen_string = "up";
+		};
+		callback(interpret_json.userTyping);
+		interpret(interpret_json);
+	};
 
-    $scope.background_image = function () {
+	$scope.background_image = function () {
 
-    };
+	};
 
-    $scope.reset = function () {
+	$scope.reset = function () {
 
-    };
+	};
 
-    var shareCallback = function (url) {
-        var image = url;
-    };
+	function shareCallback (url) {
+		var image = url;
+		// save image API
+	};
 
-    $scope.share = function () {
-        newInputData(inputDataDictionary["specialFunctions"], inputDataDictionary["saveCanvas"], shareCallback);
-    }
+	$scope.share = function () {
+		myCanvas.saveCanvas(shareCallback);
+	}
 });
 
 ExpressLOGOApp.controller('libraryViewController', function ($scope) {
@@ -75,38 +89,38 @@ ExpressLOGOApp.controller('libraryViewController', function ($scope) {
 });
 
 ExpressLOGOApp.controller('galleryViewController', function ($scope) {
-    // Set of Photos
-    $scope.photos = [
-        {src: 'http://images.17173.com/2013/news/2013/12/31/cb1231pkm21.jpg', desc: 'Image 01'},
-        {src: 'http://images.17173.com/2013/news/2013/12/31/cb1231pkm22.jpg', desc: 'Image 02'},
-        {src: 'http://images.17173.com/2013/news/2013/12/31/cb1231pkm25.jpg', desc: 'Image 03'},
-        {src: 'http://images.17173.com/2013/news/2013/12/31/cb1231pkm26.jpg', desc: 'Image 04'},
-        {src: 'http://images.17173.com/2013/news/2013/12/31/cb1231pkm27.jpg', desc: 'Image 05'},
-        {src: 'http://images.17173.com/2013/news/2013/12/31/cb1231pkm28.jpg', desc: 'Image 06'}
-    ];
+	// Set of Photos
+	$scope.photos = [
+		{src: 'http://images.17173.com/2013/news/2013/12/31/cb1231pkm21.jpg', desc: 'Image 01'},
+		{src: 'http://images.17173.com/2013/news/2013/12/31/cb1231pkm22.jpg', desc: 'Image 02'},
+		{src: 'http://images.17173.com/2013/news/2013/12/31/cb1231pkm25.jpg', desc: 'Image 03'},
+		{src: 'http://images.17173.com/2013/news/2013/12/31/cb1231pkm26.jpg', desc: 'Image 04'},
+		{src: 'http://images.17173.com/2013/news/2013/12/31/cb1231pkm27.jpg', desc: 'Image 05'},
+		{src: 'http://images.17173.com/2013/news/2013/12/31/cb1231pkm28.jpg', desc: 'Image 06'}
+	];
 
-    // initial image index
-    $scope._Index = 0;
+	// initial image index
+	$scope._Index = 0;
 
-    // if a current image is the same as requested image
-    $scope.isActive = function (index) {
-        return $scope._Index === index;
-    };
+	// if a current image is the same as requested image
+	$scope.isActive = function (index) {
+		return $scope._Index === index;
+	};
 
-    // show prev image
-    $scope.showPrev = function () {
-        $scope._Index = ($scope._Index > 0) ? --$scope._Index : $scope.photos.length - 1;
-    };
+	// show prev image
+	$scope.showPrev = function () {
+		$scope._Index = ($scope._Index > 0) ? --$scope._Index : $scope.photos.length - 1;
+	};
 
-    // show next image
-    $scope.showNext = function () {
-        $scope._Index = ($scope._Index < $scope.photos.length - 1) ? ++$scope._Index : 0;
-    };
+	// show next image
+	$scope.showNext = function () {
+		$scope._Index = ($scope._Index < $scope.photos.length - 1) ? ++$scope._Index : 0;
+	};
 
-    // show a certain image
-    $scope.showPhoto = function (index) {
-        $scope._Index = index;
-    };
+	// show a certain image
+	$scope.showPhoto = function (index) {
+		$scope._Index = index;
+	};
 });
 
 ExpressLOGOApp.controller('aboutViewController', function ($scope) {
@@ -117,57 +131,58 @@ ExpressLOGOApp.controller('aboutViewController', function ($scope) {
 });
 
 ExpressLOGOApp.controller('profileViewController', function ($scope) {
-    init();
-    function init() {
+	init();
+	function init() {
 
-    };
+	};
 });
 
 ExpressLOGOApp.controller('accountViewController', function ($scope, $http, global_data) {
-    $http({
-        method: 'GET',
-        url: '/account'
-    })
-    .success(function (data) {
-        $scope.user = data.local;
-        $scope.name = data.local.name;
-    })
-    .error(function (data) {
-        $scope.message = "Error";
-    });
+	$http({
+		method: 'GET',
+		url: '/account'
+	})
+	.success(function (data) {
+		$scope.user = data.local;
+		$scope.name = data.local.name;
+	})
+	.error(function (data) {
+		$scope.message = "Error";
+	});
 });
 
 ExpressLOGOApp.controller('signUpViewController', function ($scope, $http, global_data) {
-    $scope.message = [];
-    $http({
-      	method: 'GET',
-      	url: '/signup'
-    })
-    .success(function (data) {
-    	$scope.message = data.message;
-        if ($scope.message[0] == "success") {
-            global_data.logged_in = true;
-        };
-    })
-    .error(function (data) {
-    	$scope.message = "Unknown error";
-    });
+	$scope.message = [];
+	$http({
+		method: 'GET',
+		url: '/signup'
+	})
+	.success(function (data) {
+		$scope.message = data.message;
+		if ($scope.message[0] == "success") {
+			global_data.logged_in = true;
+		};
+	})
+	.error(function (data) {
+		$scope.message = "Unknown error";
+	});
 });
 
 ExpressLOGOApp.controller('signInViewController', function ($scope, $http, global_data) {
-    $scope.message = [];
-    $http({
-      	method: 'GET',
-      	url: '/login'
-    })
-    .success(function (data) {
-    	$scope.message = data.message;
-        if ($scope.message[0] == "success") {
-            global_data.logged_in = true;
-        };
-    })
-    .error(function (data) {
-    	$scope.message = "Unknown error";
-    });
+	$scope.message = [];
+	$http({
+		method: 'GET',
+		url: '/login'
+	})
+	.success(function (data) {
+		$scope.message = data.message;
+		console.log(global_data.logged_in);
+		if ($scope.message[0] == "success") {
+			global_data.logged_in = true;
+		};
+	})
+	.error(function (data) {
+		$scope.message[0] = "Unknown error";
+	});
 
 });
