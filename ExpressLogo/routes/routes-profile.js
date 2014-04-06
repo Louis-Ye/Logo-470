f6d84ca2dc5e466d836931ff2ec1aa615a815e92
ywa165 <ywa165@5506eaa1-8d03-4ba1-8c37-6516f2293ca1>
2014-04-06 00:45:44 +0000

    update account page
    git-svn-id: https://punch.cs.sfu.ca/svn/CMPT470-1141-g-gem@139 5506eaa1-8d03-4ba1-8c37-6516f2293ca1

--- ExpressLogo/routes/routes-profile.js
+++ ExpressLogo/routes/routes-profile.js
@@ -33,4 +33,28 @@ module.exports = function(app){
 			}
 		}
 	});
+
+	app.post('/account', function(req, res){
+		if (!req.user)
+			res.redirect('#');
+		else{
+
+			var user = req.user;
+			var reg = user.register;
+			var name = user[reg].name;
+			var obj = req.body.name;
+			console.log(obj);
+			console.log(name);
+
+			User.findByIdAndUpdate({
+				_id : req.user.id, register: 'local'
+			}, {
+				$set : {'User.$.name' : obj}
+			}, function(err, User){
+				if(err)
+					throw err;
+				res.redirect('/account');
+			});		
+		}
+	});
 };
