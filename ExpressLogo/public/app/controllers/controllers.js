d6d44a7ec2ea91dde3fde41eb297af6fba90a254
hongyiz <hongyiz@5506eaa1-8d03-4ba1-8c37-6516f2293ca1>
2014-04-07 19:01:10 +0000

    fix ;;
    git-svn-id: https://punch.cs.sfu.ca/svn/CMPT470-1141-g-gem@178 5506eaa1-8d03-4ba1-8c37-6516f2293ca1

--- ExpressLogo/public/app/controllers/controllers.js
+++ ExpressLogo/public/app/controllers/controllers.js
@@ -191,7 +191,7 @@ ExpressLOGOApp.controller('galleryViewController', function ($scope, $http, $fil
 		$scope.count = data.count;
 		$scope.items = data.post;
 
-	})
+	});
 //app.filter('offset', function() { 
 //return function(input, start) {
 //start = parseInt(start, 10);;
@@ -204,23 +204,23 @@ ExpressLOGOApp.controller('galleryViewController', function ($scope, $http, $fil
 		
 		$scope.prevPage = function() { 
 		if ($scope.currentPage > 0) {
-		$scope.currentPage--;; }
+		$scope.currentPage--; }
 		};
 
 
 		$scope.prevPageDisabled = function() {
-		return $scope.currentPage === 0 ? "disabled" : "";;
+		return $scope.currentPage === 0 ? "disabled" : "";
 		};
 
 
 		$scope.pageCount = function() {
-		return Math.ceil($scope.items.length/$scope.itemsPerPage)-1;;
+		return Math.ceil($scope.items.length/$scope.itemsPerPage)-1;
 		};
 
 
 		$scope.nextPage = function() {
 		if ($scope.currentPage < $scope.pageCount()) {
-		$scope.currentPage++;; }
+		$scope.currentPage++; }
 		};
 
 		$scope.nextPageDisabled = function() {
