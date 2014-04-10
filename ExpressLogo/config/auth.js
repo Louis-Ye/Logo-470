029ed4e79d2abc369dcb56e8dc83b392d355c24e
ywa165 <ywa165@5506eaa1-8d03-4ba1-8c37-6516f2293ca1>
2014-04-10 20:20:14 +0000

    facebook and twitter api update
    git-svn-id: https://punch.cs.sfu.ca/svn/CMPT470-1141-g-gem@224 5506eaa1-8d03-4ba1-8c37-6516f2293ca1

--- ExpressLogo/config/auth.js
+++ ExpressLogo/config/auth.js
@@ -3,12 +3,12 @@ module.exports = {
 	'facebookAuth' : {
 		'clientID' 		: '611459898943858', // your App ID
 		'clientSecret' 	: '7bb2e93aac0e5c73534d8df3b9f9d398', // your App Secret
-		'callbackURL' 	: 'http://localhost:3000/auth/facebook/callback'
+		'callbackURL' 	: 'http://cmpt470.csil.sfu.ca:8008/auth/facebook/callback'
 	},
 
 	'twitterAuth' : {
-		'consumerKey'	: 'on385zH6AM6hc4jIO2iDwLoA3',
-		'consumerSecret': 'uAA2dZaLpiLhUeb1NuWlXfljJsZvSZ9PfYlCRRZr3wQ53WaWsa',
-		'callbackURL'	: 'http:/localhost:3000/auth/twitter/callback'
+		'consumerKey'	: 'n5pGvFbFOpM9Z7L4LpFhqw',
+		'consumerSecret': 'bb43UZ3Nn7GQbCgAutrIptpIT2bI4MGyUhJ5OzMw',
+		'callbackURL'	: 'http://cmpt470.csil.sfu.ca:8008/auth/twitter/callback'
 	}
 };
