fc880dbe22a7f60563847f242ab90037b2b796d4
caoshanc <caoshanc@5506eaa1-8d03-4ba1-8c37-6516f2293ca1>
2014-04-10 11:08:00 +0000

    remove test console log
    git-svn-id: https://punch.cs.sfu.ca/svn/CMPT470-1141-g-gem@219 5506eaa1-8d03-4ba1-8c37-6516f2293ca1

--- ExpressLogo/public/app/controllers/playViewController.js
+++ ExpressLogo/public/app/controllers/playViewController.js
@@ -7,7 +7,6 @@ ExpressLOGOApp.controller('playViewController', function ($scope, $http) {
 			var result_pad = $('#result-pad');
 			message = message.replace(/ /g, '&nbsp;');
 			message = message.replace(/\n/g, '<br />');
-			console.log(message);
 			result_pad.append(message + "<br />");
 			result_pad = document.getElementById('result-pad');
 			result_pad.scrollTop = result_pad.scrollHeight;
