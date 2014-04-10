c72e6f41bd6140cb1e5f4a8a35abb419d07594d8
lvyuy <lvyuy@5506eaa1-8d03-4ba1-8c37-6516f2293ca1>
2014-04-10 07:26:36 +0000

    tutorial
    git-svn-id: https://punch.cs.sfu.ca/svn/CMPT470-1141-g-gem@212 5506eaa1-8d03-4ba1-8c37-6516f2293ca1

--- ExpressLogo/public/app/controllers/learnViewController.js
+++ ExpressLogo/public/app/controllers/learnViewController.js
@@ -1,7 +1,46 @@
-ExpressLOGOApp.controller('learnViewController', function ($scope, $routeParams) {
+ExpressLOGOApp.controller('learnViewController', function ($scope, $routeParams, $sce) {
 	init();
 	function init() {
-		console.log($routeParams);
-		
+		var pageNumber = parseInt(7);
+		var page_show = [false];
+		var curPage = parseInt($routeParams.param);
+		for (var i = 1; i <= pageNumber; i++) {
+			page_show.push(false);
+		}
+		page_show[curPage] = true;
+		$scope.page_show = page_show;
+		$scope.step = curPage;
+
+		var nextPage = curPage + 1;
+		if (nextPage > pageNumber) nextPage = pageNumber;
+		var previousPage = curPage - 1;
+		if (previousPage <= 0) previousPage = 1;
+		$scope.nextPage = nextPage;
+		$scope.previousPage = previousPage;
+		var html = getprogressButtonHTML(curPage, pageNumber);
+		$scope.progressButton = $sce.trustAsHtml( html );
 	};
+
+	function getprogressButtonHTML(curPage, pageNumber) {
+		var result = "";
+		var i = 1;
+		for (i = 1; i<=curPage; i++) {
+			result += getOKSignHTMLByNum(i) + " ";
+		}
+		for (; i <= pageNumber; i++) {
+			result += getOKCircleHTMLByNum(i) + " ";
+		}
+		return result;
+	}
+
+	function getOKSignHTMLByNum(num){
+		var str1 = "<a href='/#/tutorial/";
+		var str2 = "/'><span class='glyphicon glyphicon-ok-sign'></span></a>";
+		return str1 + num + str2;
+	}
+	function getOKCircleHTMLByNum(num){
+		var str1 = "<a href='/#/tutorial/";
+		var str2 = "/'><span class='glyphicon glyphicon-ok-circle'></span></a>";
+		return str1 + num + str2;
+	}
 });
