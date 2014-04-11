0c14e2bfbe0281c535bca81adc114cc4540a596b
yueyangh <yueyangh@5506eaa1-8d03-4ba1-8c37-6516f2293ca1>
2014-04-11 01:26:05 +0000

    git-svn-id: https://punch.cs.sfu.ca/svn/CMPT470-1141-g-gem@241 5506eaa1-8d03-4ba1-8c37-6516f2293ca1

---
+++ ExpressLogo/public/app/controllers/gallerylistViewController.js
@@ -0,0 +1,40 @@
+ExpressLOGOApp.controller('gallerylistViewController', function ($scope, $http, $routeParams) {
+	var str = $routeParams.param;
+	$scope.test = 1;
+	$http({
+		method: 'GET',
+		url: '/gallery/' + str
+	})
+	.success(function (data) {
+		$scope.photo = data;
+	});
+
+	function submit_comment(_id, content)
+	{
+		var xsrf = $.param({co: content});
+		$http({
+			method: 'POST',
+			url: '/gallery/'+ _id + '/comments',
+			data: xsrf,
+			headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+		})
+		.success(function (data) {
+			//success
+		})
+		.error(function (data) {
+			//failed
+		});
+	}
+
+	$scope.submit = function(){
+		var text = document.getElementById('cm').value;
+		document.getElementById('cm').value = "";
+		submit_comment($scope.photo._id,text);
+		window.location.reload(true);
+	}
+
+	$scope.back = function(){
+		document.getElementById('cm').value = "";
+		window.location.href='#/gallery';
+	}
+}
