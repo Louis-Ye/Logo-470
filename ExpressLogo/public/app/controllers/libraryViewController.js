7deff1ebdbb0228260643e0b2dc7ea1bd798bb50
caoshanc <caoshanc@5506eaa1-8d03-4ba1-8c37-6516f2293ca1>
2014-04-12 12:42:30 +0000

    still working on fix anchor tag
    git-svn-id: https://punch.cs.sfu.ca/svn/CMPT470-1141-g-gem@351 5506eaa1-8d03-4ba1-8c37-6516f2293ca1

--- ExpressLogo/public/app/controllers/libraryViewController.js
+++ ExpressLogo/public/app/controllers/libraryViewController.js
@@ -1,7 +1,6 @@
 ExpressLOGOApp.controller('libraryViewController', function ($scope, $location, $anchorScroll, libraryCommands) {
 	$scope.commands = libraryCommands.getCommands();
 	$scope.jumpToCommand = function (command) {
-		console.log(command);
 		$location.hash(command);
 		$anchorScroll();
 	}
